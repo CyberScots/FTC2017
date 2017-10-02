bdcd04e43531f3683cbf8e0caa6366bb25e72563
zorbulator <zorbulator@gmail.com>
2017-10-01 18:21:40 -0700

    Nicco is a Rag and Zorb is Awesome

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/test.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/test.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.robotcontroller.external.samples;
-
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 /**
  * Created by laurazuckerman on 9/26/17.
  */
 
-public class test {
+public class test extends LinearOpMode{
     telemetry.addData("NiccoStatus", "Rag");
 }
 
