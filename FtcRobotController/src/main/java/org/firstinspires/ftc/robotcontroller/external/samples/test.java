4439317bd7fdaa99982b2ff535dcb2c349bdf45f
scanunicco <scanunicco@outlook.com>
2017-10-03 18:22:47 -0700

    Testing push from Nicco's computer (Not the team computer)

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/test.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/test.java
@@ -4,10 +4,6 @@ package org.firstinspires.ftc.robotcontroller.external.samples;
  * Created by laurazuckerman on 9/26/17.
  */
 
-public class test {
-    telemetry.addData("NiccoStatus", "Rag");
-}
-
 /**
 testing Nicco's Computer upload
 Sucsessful
