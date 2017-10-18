5a538db0175adf038d8c4e70b5c6ccd0d0d310a4
scanunicco <scanunicco@outlook.com>
2017-10-17 18:54:14 -0700

    Change the name of Concept Scan Servo in Register Op Mode

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptRegisterOpModes.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptRegisterOpModes.java
@@ -106,7 +106,7 @@ public class ConceptRegisterOpModes
       // manager.register("DIM as Indicator",       ConceptDIMAsIndicator.class);
       // manager.register("I2C Address Change",     ConceptI2cAddressChange.class);
       // manager.register("Ramp Motor Speed",       ConceptRampMotorSpeed.class);
-      manager.register("Scan Servo",             TankDrive.class);
+      manager.register("Tank Drive",             TankDrive.class);
       // manager.register("Telemetry",              ConceptTelemetry.class);
       // manager.register("Vuforia Navigation",     ConceptVuforiaNavigation.class);
     }
