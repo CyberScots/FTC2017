63830f340736924320e3616a6072dd6864cf05f5
Ben <benjamin.k.mcinnis@gmail.com>
2017-10-17 18:43:58 -0700

    done

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotTeleopPOV_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotTeleopPOV_Linear.java
@@ -68,7 +68,6 @@ public class PushbotTeleopPOV_Linear extends LinearOpMode {
         double turn;
         double max;
 
-
         /* Initialize the hardware variables.
          * The init() method of the hardware class does all the work here
          */
