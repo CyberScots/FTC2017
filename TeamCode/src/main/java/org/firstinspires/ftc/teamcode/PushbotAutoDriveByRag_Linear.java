3b1526f304227dfb61a1fde7d9ff077fcecd1a33
laurazuckerman <benjamin.k.mcinnis@gmail.com>
2017-10-23 17:05:57 -0700

    here

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotAutoDriveByRag_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotAutoDriveByRag_Linear.java
@@ -55,7 +55,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  *
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
+ */test
 
 @Autonomous(name="Pushbot: Auto Drive By Rag", group="Pushbot")
 // 9*/9--+@Disabled
