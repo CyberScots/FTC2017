6c0db7456c0ce6d1a8f3581ee784a3ae68230b67
laurazuckerman <benjamin.k.mcinnis@gmail.com>
2017-11-09 18:46:54 -0800

    AddOverrideButtons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackDrive.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -34,22 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.Range;
-
-/**
- * This OpMode scans a single servo back and forwards until Stop is pressed.
- * The code is structured as a LinearOpMode
- * INCREMENT sets how much to increase/decrease the servo position each cycle
- * CYCLE_MS sets the update period.
- *
- * This code assumes a Servo configured with the name "left claw" as is found on a pushbot.
- *
- * NOTE: When any servo position is set, ALL attached servos are activated, so ensure that any other
- * connected servos are able to move freely before running this test.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 @TeleOp(name = "JackDrive", group = "Cyber Scots")
 //@Disabled
 public class JackDrive extends LinearOpMode {
@@ -58,6 +15,8 @@ public class JackDrive extends LinearOpMode {
     static final double MAX_POS     =  1.0;     // Maximum rotational position
     static final double MIN_POS     =  0.0;     // Minimum rotational position
     double CLAW_POS = 0;
+    TouchSensor lefttouch;
+    TouchSensor righttouch;
     ColorSensor colorSensor;
     // Define class members
     public DcMotor leftDrive   = null;
@@ -67,6 +26,7 @@ public class JackDrive extends LinearOpMode {
     public Servo rightHand = null;
     double  motorPowerL = 0;
     double motorPowerR = 0;
+    double motorarm = 0;
 
 
     //@Override
@@ -79,8 +39,8 @@ public class JackDrive extends LinearOpMode {
         arm  = hardwareMap.get(DcMotor.class, "left_arm");
         leftHand  = hardwareMap.get(Servo.class, "left_hand");
         rightHand  = hardwareMap.get(Servo.class, "right_hand");
-        leftDrive.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
         arm.setDirection(DcMotor.Direction.FORWARD);
         leftHand.setPosition(0.5);
         rightHand.setPosition(0.5);
@@ -91,9 +51,11 @@ public class JackDrive extends LinearOpMode {
 
 
         while(opModeIsActive()){
-
-            motorPowerL = gamepad1.left_trigger; // original code
+            lefttouch = hardwareMap.get(TouchSensor.class, "Sensor_left");
+            righttouch = hardwareMap.get(TouchSensor.class, "Sensor_right");
+            motorPowerL = gamepad1.left_trigger;
             motorPowerR = gamepad1.right_trigger;
+            motorarm = gamepad1.right_stick_y;
 
             if (Math.abs(motorPowerL) < 0.05) {
                 motorPowerL = 0;
@@ -101,18 +63,18 @@ public class JackDrive extends LinearOpMode {
             if (Math.abs(motorPowerR) < 0.05) {
                 motorPowerR = 0;
             }
-            if (gamepad1.y) {
-                arm.setPower(0.5);
-            }else if (gamepad1.a) {
-                arm.setPower(-0.5);
-            }else {
+            if (gamepad1.right_stick_y > 0.05) {
                 arm.setPower(0);
+            }else {
+                arm.setPower(motorarm);
             }
 
-            CLAW_POS += (gamepad1.right_trigger - gamepad1.left_trigger)/4;
-            CLAW_POS = Range.clip(CLAW_POS, 0, 0.5);
-            leftHand.setPosition(0.5 + CLAW_POS);
-            rightHand.setPosition(0.5 - CLAW_POS);
+            if (!lefttouch.isPressed()&& !righttouch.isPressed()) {
+                CLAW_POS += (gamepad1.left_stick_x)/4;
+                CLAW_POS = Range.clip(CLAW_POS, 0, 0.5);
+                leftHand.setPosition(0 + CLAW_POS);
+                rightHand.setPosition(0 - CLAW_POS);
+            };
 
             // Display the current value
             telemetry.addData(">", "Press Stop to end test." );
